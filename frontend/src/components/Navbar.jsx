a9ea1a01a30b4d32eb8cc9190630ecbd856abc46
Amit Gupta <amit.gupta.work12@gmail.com>
2024-10-10 00:57:56 +0530

    navbar responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+// import React from 'react'
+import { useState } from 'react'
 import { assets } from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const [visible, setvisible] = useState(false)
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
             <img src={assets.logo} alt='logo' className='w-36' />
@@ -29,6 +32,41 @@ const Navbar = () => {
 
                 </NavLink>
             </ul>
+
+            <div className='flex items-center gap-6'>
+                <img src={assets.search_icon} alt='search-icon' className='w-5 cursor-pointer' />
+                <div className='group relative'>
+
+                    <img src={assets.profile_icon} alt='search-icon' className='w-5 cursor-pointer' />
+                    <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                        <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                            <p className='cursor-pointer hover:text-black'>My Profile</p>
+                            <p className='cursor-pointer hover:text-black'>Orders</p>
+                            <p className='cursor-pointer hover:text-black'>Logout </p>
+                        </div>
+                    </div>
+                </div>
+                <Link to='/cart' className='relative'>
+                    <img src={assets.cart_icon} alt="cart-icon" className='w-5 min-w-5' />
+                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+                </Link>
+                <img onClick={() => setvisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' />
+            </div>
+
+
+            {/* Sidebar menu for small screen */}
+            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? "w-full " : "w-0"}`}>
+                <div className='flex flex-col text-gray-600'>
+                    <div className='flex items-center gap-4 p-3'>
+                        <img src={assets.dropdown_icon} className='h-4 rotate-180' alt='dropdown-icon' />
+                        <p> Back</p>
+
+                    </div>
+                </div>
+
+            </div>
+
+
         </div>
     )
 }
